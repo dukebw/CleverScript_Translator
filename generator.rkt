db6297887f45c7a8da1af6b20d7614322339b637
Brendan Duke <dukebw@mcmaster.ca>
2014-12-24 12:48:19 -0500

    Program works

--- generator.rkt
+++ generator.rkt
@@ -2,65 +2,70 @@
 
 (provide sentence
          phrase
-         find-combos
-         print-language-model)
-
-(require racket/generic)
-
-;; For finding all different possible strings from different inputs.
-(define-generics combinable
-  (find-combos combinable)
-  #:defaults 
-  ([string?
-     (define (find-combos str) (list str))]))
+         combine)
 
 ;; Define sentence -- it is an ordered list of text and phrases.
 ;; A sentence is whatever exists between two '/' separation characters in the
 ;; CleverScript file.
-(struct sentence (lst)
-        #:methods gen:combinable
-        [(define (find-combos s) (stringify (sentence-lst s)))])
+(struct sentence (lst) #:transparent)
 
 ;; Phrases contain a symbol (label) that corresponds to a sentence in 
 ;; phrase-ht.
-(struct phrase (lst) 
-        #:methods gen:combinable 
-        [(define/generic super-combos find-combos)
-         (define (find-combos p) 
-           (define (all-strings? phr) 
-             (andmap (λ (x) (string? x)) (phrase-lst phr)))
-           (cond 
-             [(all-strings? p) (flatten (phrase-lst p))]
-             [else (flatten (map super-combos (phrase-lst p)))]))])
+(struct phrase (lst) #:transparent) 
 
-;; Sentence-lst input only. This function turns a list of phrases, sentences
-;; and strings into an equivalent list of strings.
-(define (stringify lst)
-  (define (combine str-accum lst)
-    (cond
-      [(empty? lst) empty]
-      [else (cons (string-append str-accum (car lst))
-                  (combine str-accum (cdr lst)))]))
-  (define (combine-lst lst1 lst2)
-    (cond
-      [(empty? lst1) empty]
-      [else (append (combine (car lst1) lst2)
-                    (combine-lst (cdr lst1) lst2))]))
-  (cond
-    [(empty? lst) empty]
-    [(equal? (length lst) 1) (find-combos (car lst))]
-    [(string? (car lst)) (combine 
-                           (car lst) 
-                           (find-combos (sentence (cdr lst))))]
-    [else (combine-lst 
-            (find-combos (car lst)) 
-            (find-combos (sentence (cdr lst))))]))
-
-;; Output a list of strings to file.
-(define (print-language-model lst filename)
-  (define (lst-to-string lst)
-    (string-join lst "\n"
-                 #:after-last "\n"))
-  (call-with-output-file filename
-                         #:exists 'truncate
-                         (λ (out) (display (lst-to-string lst) out))))
+;; Produces a list of strings representing the language model of a sentence.
+(define (combine my-sentence)
+  (define (combine-string-list lst)
+    (define (combine-iter accum remaining)
+      (cond [(empty? remaining) accum]
+            [else (combine-iter 
+                    (string-append accum (car remaining))
+                    (cdr remaining))]))
+    (combine-iter "" lst))
+  ;; Extracts the (combined) sentences from a phrase as a list of strings.
+  (define (extract-sentences phr)
+    (define (extract-iter accum remaining)
+      (define (all-strings? lst)
+        (andmap (λ (x) (string? x)) lst))
+      (cond
+        [(empty? remaining) accum]
+        [(all-strings? (sentence-lst (car remaining))) 
+         (extract-iter 
+           (cons (combine-string-list (sentence-lst (car remaining)))
+                 accum)
+           (cdr remaining))]
+        [else (extract-iter
+                (append 
+                  accum 
+                  (combine (car remaining)))
+                (cdr remaining))]))
+    (extract-iter empty (phrase-lst phr)))
+  ;; For lists of N and M strings, returns N*M strings each of M appended to
+  ;; all of the original N strings.
+  (define (append-phrase orig-strs new-strs)
+    (define (append-phrase-iter orig-strs accum remaining)
+      (cond
+        [(empty? remaining) accum]
+        [else 
+          (define new-accum 
+            (append accum 
+                    (map (λ (str) (string-append str (car remaining))) 
+                         orig-strs)))
+          (append-phrase-iter orig-strs new-accum (cdr remaining))]))
+    (append-phrase-iter orig-strs empty new-strs))
+  (define (combine-iter accum remaining)
+    (cond 
+      [(empty? remaining) accum]
+      [(string? (car remaining)) 
+       (begin
+        (define new-accum 
+          (map (λ (str) (string-append str (car remaining))) 
+               accum))
+        (combine-iter new-accum (cdr remaining)))]
+      [(phrase? (car remaining))
+       (begin
+         (define phrase-strings (extract-sentences (car remaining))) 
+         (define new-accum (append-phrase accum phrase-strings)) 
+         (combine-iter new-accum (cdr remaining)))]
+      [else (error "Element besides string or phrase in sentence.")]))
+  (combine-iter (list "") (sentence-lst my-sentence)))
